bccc8b5719218edc66ea445e9faef8fd30a92312
kyrielin <kyrielin@tencent.com>
2021-10-31 22:40:34 +0800

    fix: github pages assets error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/visible-algorithm/' : './',
+
   server: {
     host: '0.0.0.0',
     port: 7777,
